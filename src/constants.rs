7fa2430adf03734c859cd6570af9253ff4dc06b8
kent-3 <100624004+kent-3@users.noreply.github.com>
2024-08-06 20:56:57 -0400

    checkpoint

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,3 @@
 pub static CHAIN_ID: &str = "secret-4";
 pub static LCD_URL: &str = "https://lcd.mainnet.secretsaturn.net";
-// pub static GRPC_URL: &str = "http://localhost:9091";
 pub static GRPC_URL: &str = "https://grpc.mainnet.secretsaturn.net";
--- src/keplr/tests.rs
+++ src/keplr/tests.rs
@@ -1,7 +1,6 @@
 use ::keplr::*;
 use js_sys::Error;
 use keplr_sys::disable;
-use leptos::html::Dialog;
 use leptos::prelude::*;
 use leptos::web_sys::console;
 
@@ -22,7 +21,7 @@ async fn enable_keplr(chain_id: &str) -> Result<(), String> {
     });
     match result {
         Ok(_) => log!("Keplr is enabled."),
-        Err(ref e) => log!("{e}"),
+        Err(ref e) => error!("{e}"),
     }
     result
 }
@@ -41,11 +40,20 @@ async fn get_account(chain_id: &str) -> Account {
 }
 
 async fn get_key(chain_id: &str) -> KeyInfo {
-    let key_info = Keplr::get_key(chain_id).await;
+    let result = Keplr::get_key(chain_id).await.map_err(|js_value| {
+        let error = Error::from(js_value)
+            .message()
+            .as_string()
+            .unwrap_or("unknown error".to_string());
+        error
+    });
 
-    log!("{key_info:#?}");
+    match result {
+        Ok(ref key_info) => log!("{key_info:#?}"),
+        Err(ref e) => error!("{e}"),
+    }
 
-    key_info
+    result.unwrap_or_default()
 }
 
 fn get_enigma_utils(chain_id: &str) -> () {
@@ -64,6 +72,27 @@ async fn get_secret_20_viewing_key(chain_id: &str, contract_address: String) -> 
     key
 }
 
+async fn suggest_token(chain_id: &str, contract_address: &str) {
+    let _ = ::keplr::suggest_token(chain_id, contract_address).await;
+
+    // if you want to handle the error case (user closes the pop up):
+
+    // let result = ::keplr::suggest_token(chain_id, contract_address)
+    //     .await
+    //     .map_err(|js_value| {
+    //         let error = Error::from(js_value)
+    //             .message()
+    //             .as_string()
+    //             .unwrap_or("unknown error".to_string());
+    //         error
+    //     });
+    //
+    // match result {
+    //     Ok(_) => log!("token added?"),
+    //     Err(ref e) => error!("{e}"),
+    // }
+}
+
 #[component]
 pub fn KeplrTests() -> impl IntoView {
     let enable_keplr_action: Action<(), std::result::Result<(), String>, SyncStorage> =
@@ -77,6 +106,12 @@ pub fn KeplrTests() -> impl IntoView {
             let token_address = input.clone();
             get_secret_20_viewing_key("secret-4", token_address)
         });
+    let suggest_token_action: Action<(), (), SyncStorage> = Action::new_unsync(move |_: &()| {
+        suggest_token("secret-4", "secret1s09x2xvfd2lp2skgzm29w2xtena7s8fq98v852")
+    });
+
+    let suggest_chain_action: Action<(), (), SyncStorage> =
+        Action::new_unsync(move |_: &()| suggest());
 
     let enable_keplr = move |_| enable_keplr_action.dispatch(());
     let get_account = move |_| get_account_action.dispatch(());
@@ -84,6 +119,8 @@ pub fn KeplrTests() -> impl IntoView {
     let get_viewing_key = move |_| {
         get_viewing_key_action.dispatch("secret1vkq022x4q8t8kx9de3r84u669l65xnwf2lg3e6".to_string())
     };
+    let suggest_token = move |_| suggest_token_action.dispatch_local(());
+    let suggest_chain = move |_| suggest_chain_action.dispatch_local(());
 
     // non-Actions
     let get_enigma_utils = move |_| get_enigma_utils("secret-4");
@@ -96,14 +133,9 @@ pub fn KeplrTests() -> impl IntoView {
     let pending_enable = enable_keplr_action.pending();
 
     // the most recent returned result
-    let enabled = enable_keplr_action.value();
     let account = get_account_action.value();
     let key = get_key_action.value();
     let viewing_key = get_viewing_key_action.value();
-
-    // how many times the action has run
-    // useful for reactively updating something else in response to a `dispatch` and response
-    let version = enable_keplr_action.version();
 
     view! {
         <h2>"Keplr Tests"</h2>
@@ -113,6 +145,9 @@ pub fn KeplrTests() -> impl IntoView {
             <button on:click=enable_keplr > "Enable" </button>
             <code class="font-mono max-w-max"> "window.keplr.enable(CHAIN_ID)" </code>
 
+            <button on:click=suggest_chain > "Suggest Chain" </button>
+            <code class="font-mono max-w-max"> "window.keplr.experimentalSuggestChain(...)" </code>
+
             <button on:click=get_account > "Get Account" </button>
             <code class="font-mono max-w-max"> "keplrOfflineSigner.getAccounts(CHAIN_ID)"</code>
 
@@ -121,6 +156,9 @@ pub fn KeplrTests() -> impl IntoView {
 
             <button on:click=get_enigma_utils > "Get Enigma Utils" </button>
             <code class="font-mono max-w-max"> "window.keplr.getEnigmaUtils(CHAIN_ID)"</code>
+
+            <button on:click=suggest_token > "Suggest Token (AMBER)" </button>
+            <code class="font-mono max-w-max"> "window.keplr.suggestToken(CHAIN_ID, contract_address)"</code>
 
             <button on:click=get_viewing_key > "Get Viewing Key (USDC)" </button>
             <code class="font-mono max-w-max"> "window.keplr.getSecret20ViewingKey(CHAIN_ID, contract_address)"</code>
@@ -146,4 +184,51 @@ pub fn KeplrTests() -> impl IntoView {
             </dialog>
         </Show>
     }
+}
+
+use ::keplr::suggest_chain_types::*;
+use wasm_bindgen::UnwrapThrowExt;
+
+pub async fn suggest() {
+    let chain_info = SuggestingChainInfo {
+        chain_id: "mychain-1".to_string(),
+        chain_name: "my new chain".to_string(),
+        rpc: "http://123.456.789.012:26657".to_string(),
+        rest: "http://123.456.789.012:1317".to_string(),
+        bip44: Bip44 { coin_type: 118 },
+        bech32_config: Bech32Config {
+            bech32_prefix_acc_addr: "cosmos".to_string(),
+            bech32_prefix_acc_pub: "cosmospub".to_string(),
+            bech32_prefix_val_addr: "cosmosvaloper".to_string(),
+            bech32_prefix_val_pub: "cosmosvaloperpub".to_string(),
+            bech32_prefix_cons_addr: "cosmosvalcons".to_string(),
+            bech32_prefix_cons_pub: "cosmosvalconspub".to_string(),
+        },
+        currencies: vec![Currency {
+            coin_denom: "ATOM".to_string(),
+            coin_minimal_denom: "uatom".to_string(),
+            coin_decimals: 6,
+            coin_gecko_id: "cosmos".to_string(),
+        }],
+        fee_currencies: vec![FeeCurrency {
+            coin_denom: "ATOM".to_string(),
+            coin_minimal_denom: "uatom".to_string(),
+            coin_decimals: 6,
+            coin_gecko_id: "cosmos".to_string(),
+            gas_price_step: GasPriceStep {
+                low: 0.01,
+                average: 0.025,
+                high: 0.04,
+            },
+        }],
+        stake_currency: Currency {
+            coin_denom: "ATOM".to_string(),
+            coin_minimal_denom: "uatom".to_string(),
+            coin_decimals: 6,
+            coin_gecko_id: "cosmos".to_string(),
+        },
+    };
+
+    let chain_info_js = serde_wasm_bindgen::to_value(&chain_info).unwrap();
+    let _ = suggest_chain(chain_info_js).await;
 }
